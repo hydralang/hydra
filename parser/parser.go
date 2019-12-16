7666a1a3605caf3adb515bc4918fdc8810cce179
Kevin L. Mitchell <klmitch@mit.edu>
2019-12-16 06:17:45 -0600

    Basic parser setup
    This commit contains some basic code for implementing the parser, along
    with some utilities for constructing ExprFirst and ExprNext functions.

---
+++ parser/parser.go
@@ -0,0 +1,94 @@
+// Copyright (c) 2019 Kevin L. Mitchell
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you
+// may not use this file except in compliance with the License.  You
+// may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.  See the License for the specific language governing
+// permissions and limitations under the License.
+
+package parser
+
+import (
+	"errors"
+
+	"github.com/hydralang/hydra/parser/common"
+	"github.com/hydralang/hydra/parser/lexer"
+)
+
+// parser is an implementation of Parser.
+type parser struct {
+	l    common.Lexer    // The lexer for the source
+	opts *common.Options // The parser options
+}
+
+// Parse prepares a new parser from the parser options and the lexer.
+// If the lexer is nil, one will be constructed from the options.
+func Parse(opts *common.Options, l common.Lexer) (common.Parser, error) {
+	// Construct the lexer
+	if l == nil {
+		var err error
+		l, err = lexer.Lex(opts, nil)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Construct the parser object
+	p := &parser{
+		l:    l,
+		opts: opts,
+	}
+
+	return p, nil
+}
+
+// Expression parses a single expression from the output of the lexer.
+// Note that this does not necessarily consume the entire input.  The
+// method is called with a "right binding power", which is used to
+// determine operator precedence.  An initial call should set this
+// parameter to 0; calls by token parsers (see ParserTable) may pass
+// different values, typically their left binding power.
+func (p *parser) Expression(rbp int) (common.Expression, error) {
+	// Get the parse table
+	pt := p.opts.Prof.ParseTab
+
+	// First, grab a token off the lexer
+	tok := p.l.Next()
+
+	// Do the evaluation of the first expression
+	expr, err := pt.ExprFirst(p, tok)
+	if err != nil {
+		p.l.Push(tok)
+		return nil, err
+	}
+
+	// Now handle the rest of the input
+	for tok = p.l.Next(); rbp < pt.BindingPower(p, tok); tok = p.l.Next() {
+		expr, err = pt.ExprNext(p, expr, tok)
+		if err != nil {
+			p.l.Push(tok)
+			return nil, err
+		}
+	}
+
+	p.l.Push(tok)
+	return expr, nil
+}
+
+// Statement parses a single statement from the output of the lexer.
+// Note that this does not necessarily consume the entire input.
+func (p *parser) Statement() (common.Statement, error) {
+	return nil, errors.New("not implemented")
+}
+
+// Module parses a module, or collection of statements, from the
+// output of the lexer.  This is intended to consume the entire input.
+func (p *parser) Module() (common.Statement, error) {
+	return nil, errors.New("not implemented")
+}
---
+++ parser/utils.go
@@ -0,0 +1,82 @@
+// Copyright (c) 2019 Kevin L. Mitchell
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you
+// may not use this file except in compliance with the License.  You
+// may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.  See the License for the specific language governing
+// permissions and limitations under the License.
+
+package parser
+
+import (
+	"github.com/hydralang/hydra/parser/ast"
+	"github.com/hydralang/hydra/parser/common"
+)
+
+// LiteralExprFirst is an ExprFirst function that returns a literal
+// value.
+func LiteralExprFirst(p common.Parser, t *common.Token) (common.Expression, error) {
+	return &ast.Constant{
+		Loc: t.Loc,
+		Val: t.Val,
+	}, nil
+}
+
+// InfixExprNext is a factory function for ExprNext functions for
+// basic infix operators--e.g., "+", "-", "*", "/", etc.
+func InfixExprNext(op string, lbp int) ExprNext {
+	return func(p common.Parser, l common.Expression, r *common.Token) (common.Expression, error) {
+		right, err := p.Expression(lbp)
+		if err != nil {
+			return nil, err
+		}
+
+		return &ast.Binary{
+			Loc:   l.GetLoc().ThruEnd(r.Loc),
+			Op:    op,
+			Left:  l,
+			Right: right,
+		}, nil
+	}
+}
+
+// PrefixExprFirst is a factory function for ExprFirst functions for
+// prefixed unary operators--e.g., "~", "-", etc.
+func PrefixExprFirst(op string, lbp int) ExprFirst {
+	return func(p common.Parser, t *common.Token) (common.Expression, error) {
+		expr, err := p.Expression(lbp)
+		if err != nil {
+			return nil, err
+		}
+
+		return &ast.Unary{
+			Loc:  expr.GetLoc(),
+			Op:   op,
+			Node: expr,
+		}, nil
+	}
+}
+
+// InfixRightExprNext is a factory function for ExprNext functions for
+// right-associative infix operators--e.g., "**".
+func InfixRightExprNext(op string, lbp int) ExprNext {
+	return func(p common.Parser, l common.Expression, r *common.Token) (common.Expression, error) {
+		right, err := p.Expression(lbp - 1)
+		if err != nil {
+			return nil, err
+		}
+
+		return &ast.Binary{
+			Loc:   l.GetLoc().ThruEnd(r.Loc),
+			Op:    op,
+			Left:  l,
+			Right: right,
+		}, nil
+	}
+}
